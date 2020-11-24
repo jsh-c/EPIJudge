8239a4f32eaad4aac14213fb0e70e6da15995006
repl.it user <replituser@example.com>
2020-11-24 07:40:31 +0000

    epi_judge_java/epi/BuyAndSellStock.java

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -7,7 +7,13 @@ public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
     // TODO - you fill in here.
-    return 0.0;
+    double maxProfit = 0;
+    double minPrice = prices.get(0);
+    for (int i=1; i<prices.size(); i++) {
+      maxProfit = Math.max(maxProfit, prices.get(i)-minPrice);
+      minPrice = Math.min(minPrice, prices.get(i));
+    }
+    return maxProfit;
   }
 
   public static void main(String[] args) {
